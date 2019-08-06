aad18ebf61245ac86bdd5c45cfc03e286d2b06c9
Logon Aluno <logonaluno@local.com>
2019-08-05 22:27:47 -0300

    Editing activity

--- app/src/main/java/br/com/fiap/rodrigo/imc/MainActivity.kt
+++ app/src/main/java/br/com/fiap/rodrigo/imc/MainActivity.kt
@@ -1,7 +1,9 @@
 package br.com.fiap.rodrigo.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnCalculate.setOnClickListener {
+            val nextScreen = Intent(this, ResultActivity::class.java)
 
+            nextScreen.putExtra("WEIGHT", inputWeight.text.toString())
+            nextScreen.putExtra("HEIGHT", inputHeight.text.toString())
+            nextScreen.putExtra("SEX", spSex.selectedItem.toString())
+
+            startActivity(nextScreen)
+        }
+
+        btnClear.setOnClickListener {
+            inputWeight.setText("")
+            inputHeight.setText("")
+            spSex.setSelection(0)
+        }
     }
 }
